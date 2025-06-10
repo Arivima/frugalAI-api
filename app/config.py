3c3f82a9557e3365f28fbd564d31559fbecb2fa8
Arivima <arielle.villamassone@gmail.com>
2025-06-10 17:27:05 +0200

    clean

--- app/config.py
+++ app/config.py
@@ -1,11 +1,10 @@
-# app/config.py
 import os
 import logging
 
 class Config:
     GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID", "")
-    BQ_DATASET_ID = os.getenv("BQ_DATASET_ID", "climate_feedback")
-    BQ_TABLE_ID = os.getenv("BQ_TABLE_ID", "user_feedback")
+    BQ_DATASET_ID = os.getenv("BQ_DATASET_ID", "")
+    BQ_TABLE_ID = os.getenv("BQ_TABLE_ID", "")
     GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME", "")
     DESTINATION_DIRECTORY = os.getenv("DESTINATION_DIRECTORY", "")
     ADAPTER_NAME = os.getenv("ADAPTER_NAME", "")
